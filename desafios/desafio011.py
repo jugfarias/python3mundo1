403be8af7adcfd7c13b08d67300fc51d3792a797
jugfarias <jufarias13@gmail.com>
2024-09-15 14:15:54 -0300

    desafio 11 - pintar parede

---
+++ desafios/desafio011.py
@@ -0,0 +1,12 @@
+# Faça um programa que leia a altura e a largura de uma parede, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m².
+
+import math
+
+larg = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
+
+area = larg * alt
+
+litros = area / 2
+
+print('É necessário {}L de tinta para pintar {}m² de parede.'.format(math.ceil(litros), area))
