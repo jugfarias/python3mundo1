92beebb1bdd44d94f5716595ea1976eb6f393d71
jugfarias <jufarias13@gmail.com>
2024-09-15 15:33:42 -0300

    uso de from math import ceil

--- desafios/desafio011.py
+++ desafios/desafio011.py
@@ -1,6 +1,6 @@
 # Faça um programa que leia a altura e a largura de uma parede, calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2m².
 
-import math
+from math import ceil
 
 larg = float(input('Largura da parede: '))
 alt = float(input('Altura da parede: '))
@@ -9,4 +9,4 @@ area = larg * alt
 
 litros = area / 2
 
-print('É necessário {}L de tinta para pintar {}m² de parede.'.format(math.ceil(litros), area))
+print('É necessário {}L de tinta para pintar {}m² de parede.'.format(ceil(litros), area))
