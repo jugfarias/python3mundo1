183efa9c4240a27565d3275210236a720ab19f1c
jugfarias <jufarias13@gmail.com>
2024-09-16 14:00:59 -0300

    desafio 24 - lower, split, if else

---
+++ desafios/desafio024.py
@@ -0,0 +1,11 @@
+# Crie um programa que leia um nome de uma cidade e diga se ela começa ou não com o nome "Santo".
+
+cidade = input('Digite o nome de uma cidade: ')
+cidade = cidade.lower()
+
+cidade_lista = cidade.split()
+
+if cidade_lista[0] == 'santo':
+    print('A cidade começa com Santo')
+else:
+    print('A cidade não começa com Santo')
