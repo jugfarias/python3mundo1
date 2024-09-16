1ad131b3ea3eb3aafee794e7fae9aa35f202d0df
jugfarias <jufarias13@gmail.com>
2024-09-16 18:53:54 -0300

    desafio 24 - adição de forma mais simples sem condicional

--- desafios/desafio024.py
+++ desafios/desafio024.py
@@ -2,10 +2,12 @@
 
 cidade = input('Digite o nome de uma cidade: ')
 cidade = cidade.lower()
-
+'''
 cidade_lista = cidade.split()
 
 if cidade_lista[0] == 'santo':
     print('A cidade começa com Santo')
 else:
     print('A cidade não começa com Santo')
+'''
+print(cidade[0:5] == "santo")
