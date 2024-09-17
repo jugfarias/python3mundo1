aaa818f76bdc4eb28600d30ed303c90242952f9a
jugfarias <jufarias13@gmail.com>
2024-09-17 13:14:14 -0300

    desafio 26 - correção

--- desafios/desafio026.py
+++ desafios/desafio026.py
@@ -5,7 +5,8 @@
 
 frase = input('Digite uma frase: ')
 frase = frase.lower()
+frase = frase.strip()
 
 print('Quantidade de letras a na frase: {}'.format(frase.count('a')))
-print('Primeira aparição da letra a foi na posição: {}'.format(frase.find('a')))
-print('Última aparição da letra a foi na posição: {}'.format(frase.rfind('a')))
+print('Primeira aparição da letra a foi na posição: {}'.format(frase.find('a') + 1))
+print('Última aparição da letra a foi na posição: {}'.format(frase.rfind('a') + 1))
