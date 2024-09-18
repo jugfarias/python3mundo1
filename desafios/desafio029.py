8c07cb88ee92e3e719fa2fc70ca1230e2e9d6d66
jugfarias <jufarias13@gmail.com>
2024-09-17 21:28:54 -0300

    desafio 29 - multa por velocidade

---
+++ desafios/desafio029.py
@@ -0,0 +1,10 @@
+# Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada km acima do limite.
+
+velocidade = int(input('Digite a velocidade em km/h: '))
+limite = 80
+
+if velocidade > limite:
+    multa = (velocidade - limite) * 7
+    print('Você foi multado. R${},00'.format(multa))
+else:
+    print('Passar bem.')
