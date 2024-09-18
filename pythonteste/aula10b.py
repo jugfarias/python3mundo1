22d83b52372d7dd45949549c1cfaf0ee404029aa
jugfarias <jufarias13@gmail.com>
2024-09-17 21:13:40 -0300

    condicionais para cálculo de média

---
+++ pythonteste/aula10b.py
@@ -0,0 +1,10 @@
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+media = (nota1 + nota2) / 2
+
+if media >= 6:
+    print('Parabéns!')
+else:
+    print('Estude mais...')
+
+print('Parabéns!' if media >= 6 else 'Estude mais...')
