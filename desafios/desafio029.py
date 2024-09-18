c90d9118b1e5422eda999b3e9e25cf728bda3302
jugfarias <jufarias13@gmail.com>
2024-09-18 11:50:45 -0300

    int -> float

--- desafios/desafio029.py
+++ desafios/desafio029.py
@@ -1,10 +1,10 @@
 # Escreva um programa que leia a velocidade de um carro. Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele foi multado. A multa vai custar R$7,00 por cada km acima do limite.
 
-velocidade = int(input('Digite a velocidade em km/h: '))
+velocidade = float(input('Digite a velocidade em km/h: '))
 limite = 80
 
 if velocidade > limite:
     multa = (velocidade - limite) * 7
-    print('Você foi multado. R${},00'.format(multa))
+    print('Você foi multado. R${:.2f}'.format(multa))
 else:
     print('Passar bem.')
